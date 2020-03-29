756210f07386bb0dd443d7ba225576ee91d83a2f
kscript <1258@live.cn>
2020-03-29 12:53:46 +0800

    修复路径错误,增加清空输出文件夹

--- index.js
+++ index.js
@@ -17,15 +17,15 @@ const map = {
   tag: {}
 }
 const keys = {}
-Object.keys(map).some(function(item) {
+Object.keys(map).some(function (item) {
   keys[item] = {}
 })
-const countInfo = function(cached) {
-  cached.map(function(file) {
+const countInfo = function (cached) {
+  cached.map(function (file) {
     let category = countVal(map, 'category', file)
     let tag = countVal(map, 'tag', file)
-    category.map(function(c) {
-      tag.map(function(t) {
+    category.map(function (c) {
+      tag.map(function (t) {
         keys.category[c] = (keys.category[c] || []).concat(t)
       })
     })
@@ -33,14 +33,14 @@ const countInfo = function(cached) {
   return map
 }
 
-const countVal = function(target, key, file) {
+const countVal = function (target, key, file) {
   let obj = target[key]
   let val = file.config[key]
   if (typeof val === 'string') {
     val = [val]
   }
   if (val instanceof Array) {
-    return unique(val).map(function(item) {
+    return unique(val).map(function (item) {
       if (obj[item]) {
         obj[item].push(file)
       } else {
@@ -51,32 +51,30 @@ const countVal = function(target, key, file) {
   }
   return []
 }
-const countSort = function(list) {
+const countSort = function (list) {
   let obj = {}
-  list.map(function(item) {
+  list.map(function (item) {
     obj[item] = (obj[item] || 0) + 1
   })
-  return Object.keys(obj).sort(function(a, b) {
+  return Object.keys(obj).sort(function (a, b) {
     return obj[b] - obj[a]
   })
 }
-const writeConfig = function() {
+const writeConfig = function (dirname) {
   let category = []
-  Object.keys(keys.category).map(function(item) {
+  Object.keys(keys.category).map(function (item) {
     category.push([item, countSort(keys.category[item]).slice(0, conf.tagNum).join()])
   })
-  fs.writeFileSync(path.join(conf.output, '/categorys.json'), JSON.stringify(category, null, 2))
-  Object.keys(map.category).map(function(key) {
+  fs.writeFileSync(path.join(conf.output, dirname, '/categorys.json'), JSON.stringify(category, null, 2))
+  Object.keys(map.category).map(function (key) {
     let tag = map.category[key]
-    let res = tag.map(function(item) {
+    let res = tag.map(function (item) {
       return {
         title: item.config.title || item.stats.name,
         path: item.stats.path
       }
     })
-    console.log(key)
-    console.log('')
-    fs.writeFileSync(path.join(conf.output, '/' + key +'.json'), JSON.stringify(res, null, 2))
+    fs.writeFileSync(path.join(conf.output, dirname, '/' + key + '.json'), JSON.stringify(res, null, 2))
   })
 }
 const parseConfig = function (yaml, stats) {
@@ -86,12 +84,13 @@ const parseConfig = function (yaml, stats) {
       return config
     }
     console.log(config)
-    throw(new Error('配置信息读取失败!'))
-  } catch(err) {
-    throw(new Error(err))
+    throw (new Error('配置信息读取失败!'))
+  } catch (err) {
+    throw (new Error(err))
   }
 }
 const mkdirsSync = (dir) => {
+  dir = path.join(dir)
   if (fs.existsSync(dir)) {
     return true;
   } else {
@@ -112,10 +111,11 @@ const extract = function (content, type) {
   }
   return type ? resObj[type] || resObj.markdown : resObj
 }
-const unique = function(ary) {
+
+const unique = function (ary) {
   var obj = {}
   var newAry = []
-  ary.map(function(item) {
+  ary.map(function (item) {
     if (!obj[item]) {
       obj[item] = 1
       newAry.push(item)
@@ -123,54 +123,92 @@ const unique = function(ary) {
   })
   return newAry
 }
-mkdirsSync(conf.output)
-fsLoader({
-  // 要进行转换的目录
-  // 相对于项目目录, 而非文件所在目录
-  path: conf.input,
 
-  // 文件扩展名, 支持正则
-  // ext: 'md',
-
-  // 是否深层遍历
-  deep: true,
-  
-  showDir: true,
-  // 是否读取文件内容
-  readFile: true,
-  /**
-   * 加载器
-   * @param {object} stats 文件信息
-   * @param {string} data 文件内容 readFile 为 false 时返回空字符串
-   * @param {function} done 文件处理完毕的回调 (必要的)
-   */
-  loader: function(stats, data, done){
-    let res = path.parse(stats.path)
-    let relativePath = path.join(stats.path).slice(path.join(conf.input).length)
-    let outputPath = path.join(conf.output, relativePath)
-    mkdirsSync(path.parse(outputPath).dir)
-    if (stats.type === 'file') {
-      fs.writeFile(outputPath, data, function() {
-        if (res.ext === '.md') {
-          let info = extract(data)
-          let config = parseConfig(info.yaml, stats)
-          cached.push({
-            info,
-            data,
-            stats,
-            config
-          })
-        }
-        done()
-      })
-      return false
-    }
-  },
-  // 转换完毕
-  done: function(){
-    let infos = countInfo(cached.sort(function(a, b) {
-      return (a.stats[conf.sort.key] - b.stats[conf.sort.key]) * (conf.sort.desc ? -1 : 1)
-    }))
-    writeConfig()
+const getOutputPath = function (currentPath, input, output) {
+  let relativePath = path.join(currentPath).slice(path.join(input).length)
+  return path.join(output, relativePath)
+}
+const deleteFolder = function (path) {
+  let files = []
+  if (fs.existsSync(path)) {
+    files = fs.readdirSync(path)
+    files.forEach(function (file, index) {
+      let curPath = path + "/" + file
+      if (fs.statSync(curPath).isDirectory()) {
+        deleteFolder(curPath)
+      } else {
+        fs.unlinkSync(curPath)
+      }
+    })
+    fs.rmdirSync(path)
   }
-})
+}
+const build = function (done) {
+  deleteFolder(conf.output)
+  mkdirsSync(conf.output)
+  // 第一层的子目录, 是与项目对应的类目
+  // 当然, 这一层还可能有别的文件
+  // 如果是目录, 整理该类目下的信息
+  // 如果是文件, 直接复制到相应的输出目录
+  fsLoader({
+    path: conf.input,
+    deep: false,
+    showDir: true,
+    readFile: false,
+    loader(stats, data, done) {
+      if (stats.type === 'file') {
+        let outputPath = getOutputPath(stats.path, conf.input, conf.output)
+        fs.writeFile(outputPath, data, done)
+        return false
+      } else
+        if (stats.type === 'dir') {
+          let dirname = stats.name
+          mkdirsSync(path.join(conf.output, dirname))
+          fsLoader({
+            path: stats.path,
+            deep: true,
+            showDir: true,
+            readFile: true,
+            loader: function (stats, data, done) {
+              let ext = path.parse(stats.path).ext
+              let outputPath = getOutputPath(stats.path, conf.input, conf.output)
+              if (stats.type === 'dir') {
+                mkdirsSync(outputPath)
+              } else
+                if (stats.type === 'file') {
+                  mkdirsSync(path.parse(outputPath).dir)
+                  fs.writeFile(outputPath, data, function () {
+                    if (ext === '.md') {
+                      let info = extract(data)
+                      let config = parseConfig(info.yaml, stats)
+                      cached.push({
+                        info,
+                        data,
+                        stats,
+                        config
+                      })
+                    }
+                    done()
+                  })
+                  return false
+                }
+            },
+            done: function () {
+              countInfo(cached.sort(function (a, b) {
+                return (a.stats[conf.sort.key] - b.stats[conf.sort.key]) * (conf.sort.desc ? -1 : 1)
+              }))
+              writeConfig(dirname)
+              console.log(dirname + ' 类目处理完成')
+              done()
+            }
+          })
+          return false
+        }
+    },
+    done() {
+      typeof done === 'function' && done()
+      console.log("执行完毕")
+    }
+  })
+}
+build()
